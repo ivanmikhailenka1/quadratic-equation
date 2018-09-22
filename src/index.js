46132679fda0be7e22e47af40aceda9c0e8d35c7
Ivan <ivanmikhailenka1@gmail.com>
2018-09-22 23:59:52 +0300

    implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let vars = equation.match('(-?\\d+) \\* x\\^2 ([+-] \\d+) \\* x ([+-] \\d+)');
+    let a = vars[1];
+    let b = vars[2].replace(" ", "");
+    let c = vars[3].replace(" ", "");
+    let D = b * b - 4 * a * c;
+    let x1 = Math.round((-b - Math.sqrt(D)) / (2*a));
+    let x2 = Math.round((-b + Math.sqrt(D)) / (2*a));
+    return [x1, x2].sort()
 }
